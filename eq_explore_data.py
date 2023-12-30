6f44fd8517099d7de66313ca5c426d9e6698829c
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2023-12-30 22:53:22 +0400

    Extract Latitude of every earthquake

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -16,15 +16,18 @@ all_eq_dicts = all_eq_data['features']
 
 mags,lons,lats=[],[],[]
 
-# Extract Magnitude and Longitude of every Earthquake:
+# Extract Magnitude, Longitude  and Latitude of every Earthquake:
 
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
     lon = eq_dict['geometry']['coordinates'][0]
+    lan = eq_dict['geometry']['coordinates'][1]
     
     mags.append(mag)
     lons.append(lon)
+    lats.append(lan)
 
 
 print(mags[:10])
 print(lons[:10])
+print(lats[:10])
