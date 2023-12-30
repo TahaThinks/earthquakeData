94532b091609ab39415ca2d335055d759d746c88
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2023-12-31 00:17:53 +0400

    Customize Colors for better view

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -2,7 +2,7 @@ import json
 from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
-filename = 'data\eq_data_1_day_m1.json'
+filename = 'data\eq_data_30_day_m1.json'
 
 with open(filename) as f:
     all_eq_data = json.load(f)
@@ -30,13 +30,21 @@ for eq_dict in all_eq_dicts:
     lats.append(lan)
 
 
-# Map the earthquake, 'marker' icreases size:
+# Map the earthquake:
+# 'marker-size' icreases size
+# 'marker-color' choose which data to apply coloring
+# 'marker-colorscale' which color to use
+# 'marker-colorbar' add a bar for reference
 data = [{
     'type':'scattergeo',
     'lon':lons,
     'lat':lats,
     'marker':{
-        'size':[5*mag for mag in mags]
+        'size':[5*mag for mag in mags],
+        'color':mags,
+        'colorscale':'Viridis',
+        'reversescale': True,
+        'colorbar':{'title': 'Magnitude'},
     }
 }]
 my_layout = Layout(title="Global Earthquakes")
